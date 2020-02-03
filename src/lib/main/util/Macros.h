4958b3a99395c3963b14e19549f6df9efcc50dac
E-gy <balakhelisey@gmail.com>
2020-02-03 20:26:09 +0100

    Imported ForEach macros

--- src/lib/main/util/Macros.h
+++ src/lib/main/util/Macros.h
@@ -12,25 +12,10 @@ typedef const char* String;
 //Counts the number of arguments up to 255 [inclusive], supports 0
 #define VARARG_COUNT(...) VARARG_GET_256TH( 0, ##__VA_ARGS__, 255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236, 235, 234, 233, 232, 231, 230, 229, 228, 227, 226, 225, 224, 223, 222, 221, 220, 219, 218, 217, 216, 215, 214, 213, 212, 211, 210, 209, 208, 207, 206, 205, 204, 203, 202, 201, 200, 199, 198, 197, 196, 195, 194, 193, 192, 191, 190, 189, 188, 187, 186, 185, 184, 183, 182, 181, 180, 179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 165, 164, 163, 162, 161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151, 150, 149, 148, 147, 146, 145, 144, 143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133, 132, 131, 130, 129, 128, 127, 126, 125, 124, 123, 122, 121, 120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
-#define MREC_EVAL0(...) __VA_ARGS__
-#define MREC_EVAL1(...) MREC_EVAL0 (MREC_EVAL0 (MREC_EVAL0 (__VA_ARGS__)))
-#define MREC_EVAL2(...) MREC_EVAL1 (MREC_EVAL1 (MREC_EVAL1 (__VA_ARGS__)))
-#define MREC_EVAL3(...) MREC_EVAL2 (MREC_EVAL2 (MREC_EVAL2 (__VA_ARGS__)))
-#define MREC_EVAL4(...) MREC_EVAL3 (MREC_EVAL3 (MREC_EVAL3 (__VA_ARGS__)))
-#define MREC_EVAL(...)  MREC_EVAL4 (MREC_EVAL4 (MREC_EVAL4 (__VA_ARGS__)))
+#include "MacrosForeach.h"
 
-#define MREC_MAP_END(...)
-
-#define MREC_MAP_GET_END() 0, MREC_MAP_END
-#define MREC_MAP_NEXT0(item, next, ...) next MREC_MAP_OUT
-#define MREC_MAP_NEXT1(item, next) MREC_MAP_NEXT0 (item, next, 0)
-#define MREC_MAP_NEXT(item, next)  MREC_MAP_NEXT1 (MREC_MAP_GET_END item, next)
-
-#define MREC_MAP0(f, x, peek, ...) f(x) MREC_MAP_NEXT (peek, MREC_MAP1) (f, peek, __VA_ARGS__)
-#define MREC_MAP1(f, x, peek, ...) f(x) MREC_MAP_NEXT (peek, MREC_MAP0) (f, peek, __VA_ARGS__)
-
-/// Expands macro `f` on every supplied element
-#define foreachDoMacro(f, ...) MREC_EVAL (MREC_MAP1 (f, __VA_ARGS__, (), 0))
+#define FOREACH_ARG(f, ...) MREC_MAP(f, __VA_ARGS__)
+#define TRANSFORM_ARGS(f, ...) MREC_MAP_LIST(F, __VA_ARGS__)
 
 //Math
 
